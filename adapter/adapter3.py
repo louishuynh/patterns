96cb639e41964cc632a2c112ef6bf857f0f0d22f
louis <@>
2020-05-08 03:10:42 +0100

    Update to the adapter patterns and move observer.

--- adapter/adapter3.py
+++ adapter/adapter3.py
@@ -4,6 +4,9 @@ Create a very general adapter class for any minion.
 
 
 class Elf:
+    def __init__(self):
+        self.name = 'Orlando'
+
     def nall_nin(self):
         print('Elf says: Calling the Overlord...')
 
@@ -22,9 +25,10 @@ class MinionAdapter:
     __initialised = False
 
     def __init__(self, minion, **adapted_methods):
+        """ adaptered_methods is a dict mapping of new name to old name """
         self.minion = minion
         
-        for key, value in adapted_methods.items():
+        for key, value in adapted_methods.items(): 
             func = getattr(self.minion, value)  # get function from actual minion
             self.__setattr__(key, func)   # and set on the adapter
 
@@ -32,40 +36,35 @@ class MinionAdapter:
 
     def __getattr__(self, attr):
         """ Attributes not in Adapter are delegated to the minion."""
+        # You can get and set any attribute still on minion and not on Adapter.
         return getattr(self.minion, attr)
 
-    def __setattr
-
-
-
-
-class DwarfAdapter:
-    def __init__(self, dwarf):
-        self.dwarf = dwarf
-
-    def call_me(self):
-        self.dwarf.estver_narho()
-
-
-class HumanAdapter:
-    def __init__(self, human):
-        self.human = human
-
-    def call_me(self):
-        self.human.ring_mig()
-
-
-
-
-
+    def __setattr__(self, key, value):
+        """ Set attributes normally during initialisation."""
+        if not self.__initialised:
+            super().__setattr__(key, value)
+        else:
+            """ Set attribute on minion after initialisation."""
+            setattr(self.minion, key, value)
 
 
 if __name__ == '__main__':
-    minions = [
-        ElfAdapter(Elf()), 
-        DwarfAdapter(Dwarf()), 
-        HumanAdapter(Human())
-        ]
+    minion_adapters = [
+        MinionAdapter(Elf(), call_me='nall_nin'),
+        MinionAdapter(Dwarf(), call_me='estver_narho'),
+        MinionAdapter(Human(), call_me='ring_mig'),        
+    ]
 
-    for minion in minions:
-        minion.call_me()
+    for adapter in minion_adapters:
+        adapter.call_me()
+        
+    elf_adapter = minion_adapters[0]
+    print('')
+    print(f'Name from Adapter: {elf_adapter.name}')
+    print(f'Name from Minion: {elf_adapter.minion.name}')
+
+    elf_adapter.name = 'Bloom'
+
+    print('')
+    print(f'Name from Adapter: {elf_adapter.name}')
+    print(f'Name from Minion: {elf_adapter.minion.name}')
